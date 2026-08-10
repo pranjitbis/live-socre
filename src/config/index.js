d2453a10bea3354188a243da8c895e8f2993e5f3
Pranjit Biswas <pranjitbiswas77@gmail.com>
2026-08-10 12:46:39 +0530

    s

--- src/config/index.js
+++ src/config/index.js
@@ -17,6 +17,7 @@ const config = {
   },
 
   redis: {
+    user: process.env.REDIS_REDISUSER || 'default',
     host: process.env.REDIS_HOST || 'localhost',
     port: parseInt(process.env.REDIS_PORT) || 6379,
     password: process.env.REDIS_PASSWORD || undefined,
